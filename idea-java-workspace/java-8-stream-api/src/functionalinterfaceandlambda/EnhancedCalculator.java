e606593bb30fbf6190d7599c24da5860cb94dd26
Ramanuj Das <ramanujds9@gmail.com>
2024-04-29 16:48:07 +0530

    Updated with Lambda Expression and Stream API

--- idea-java-workspace/java-8-stream-api/src/functionalinterfaceandlambda/EnhancedCalculator.java
+++ idea-java-workspace/java-8-stream-api/src/functionalinterfaceandlambda/EnhancedCalculator.java
@@ -1,6 +1,13 @@
 package functionalinterfaceandlambda;
 
+import java.util.function.BinaryOperator;
+import java.util.function.BooleanSupplier;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
 public class EnhancedCalculator {
+
+
 
     // create a method calculate() that should take three arguments - two should be integers/double
     // third should be operation(some code)
---
+++ idea-java-workspace/java-8-stream-api/src/streamapi/StreamApiExample.java
@@ -0,0 +1,42 @@
+package streamapi;
+
+import java.util.List;
+import java.util.function.BinaryOperator;
+import java.util.stream.Collectors;
+
+public class StreamApiExample {
+
+    public static void main(String[] args) {
+
+        List<Integer> nums = List.of(-1, 2, -3, 5, -6, -7, 10);
+
+        // filter - find all the positive numbers
+
+        // map - double the elements
+
+        List<Integer> output = nums.stream().filter(e -> e > 0)
+                                             .map(e -> e * e)
+                                             .toList();
+
+        System.out.println(output);
+
+//        int output =
+//        nums.stream().filter(e -> {
+//                    System.out.println("Filtered " + e);
+//                    return e > 0;
+//                })
+//                .map(e -> {
+//                    System.out.println("Mapped " + e);
+//                    return e * 2;
+//                })
+//                .findFirst().get();
+//
+////                .reduce(0,(sum,e)->sum+e);
+//
+//
+//               System.out.println(output);
+
+
+    }
+
+}
