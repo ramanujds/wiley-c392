a6e6cca236208223abb299d35a0c5cc80c09b492
Ramanuj Das <ramanujds9@gmail.com>
2024-05-04 15:27:38 +0530

    Datastructures - Binary Search Tree

---
+++ idea-java-workspace/data-structures/src/ds/binarysearchtree/Node.java
@@ -0,0 +1,21 @@
+package ds.binarysearchtree;
+
+public class Node {
+
+    int key;
+    String value;
+
+    Node leftChild, rightChild;
+
+    public Node(int key, String value) {
+        super();
+        this.key = key;
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Node [key=" + key + ", value=" + value + "]";
+    }
+
+}
