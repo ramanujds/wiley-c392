bf5e85e34b0b89936c3c79eb467ca9e575784b90
Ramanuj Das <ramanujds9@gmail.com>
2024-05-24 12:51:44 +0530

    Log Statements

--- spring-workspace/trainees-app-with-jdbc-template/src/main/java/com/wiley/traineesapp/repository/TraineeRepositoryImpl.java
+++ spring-workspace/trainees-app-with-jdbc-template/src/main/java/com/wiley/traineesapp/repository/TraineeRepositoryImpl.java
@@ -3,6 +3,7 @@ package com.wiley.traineesapp.repository;
 import com.wiley.traineesapp.exception.RecordNotFoundException;
 import com.wiley.traineesapp.model.Trainee;
 import com.wiley.traineesapp.util.TraineeRowMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -13,7 +14,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
-
+@Slf4j
 @Repository
 public class TraineeRepositoryImpl implements TraineeRepository {
 
@@ -30,6 +31,7 @@ public class TraineeRepositoryImpl implements TraineeRepository {
     public Trainee saveTrainee(Trainee trainee) {
         int rowCount = jdbcTemplate.update("insert into trainees(trainee_name,email,location) values(?,?,?)",
                 trainee.getName(), trainee.getEmail(), trainee.getLocation());
+        log.info("Record Saved : "+rowCount);
         if (rowCount > 0) {
             return getTraineeByName(trainee.getName()).get();
         }
@@ -39,9 +41,11 @@ public class TraineeRepositoryImpl implements TraineeRepository {
     public Optional<Trainee> getTraineeById(int id) {
        try {
            Trainee trainee = jdbcTemplate.queryForObject("select * from trainees where id=" + id, new TraineeRowMapper());
+           log.info("Found Trainee : "+trainee.toString());
            return Optional.of(trainee);
        }
        catch (DataAccessException ex){
+           log.error("Trainee with id : "+id+" Not Found");
            throw new RecordNotFoundException("Trainee with id : "+id+" Not Found");
        }
     }
