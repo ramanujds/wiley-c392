3569a2069a7551b63274b4c689ba18d86bfd8366
Ramanuj Das <ramanujds9@gmail.com>
2024-05-24 12:34:22 +0530

    Rest API with Swagger UI

--- spring-workspace/trainees-app-with-jdbc-template/src/main/java/com/wiley/traineesapp/controller/TraineeController.java
+++ spring-workspace/trainees-app-with-jdbc-template/src/main/java/com/wiley/traineesapp/controller/TraineeController.java
@@ -4,19 +4,30 @@ import com.wiley.traineesapp.dto.TraineeDto;
 import com.wiley.traineesapp.model.Trainee;
 import com.wiley.traineesapp.service.TraineeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/trainees")
 public class TraineeController {
 
     @Autowired
     private TraineeService traineeService;
 
+    @GetMapping("/{id}")
+    public TraineeDto getTrainee(@PathVariable int id){
+        return traineeService.getTrainee(id);
+    }
 
+    @PostMapping
+    public TraineeDto saveTrainee(@RequestBody TraineeDto traineeDto){
+        return traineeService.saveTrainee(traineeDto);
+    }
 
+    @GetMapping
+    public List<TraineeDto> getAllTrainees(){
+        return traineeService.getAllTrainees();
+    }
 
 }
